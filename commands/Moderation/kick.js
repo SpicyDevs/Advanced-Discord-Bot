8cf3fc5bfba8fbdb9358d9715904bf25b4ba3dff
Coderboi4 <103612036+Coderboi4@users.noreply.github.com>
2023-05-29 18:01:11 +1000

    Update kick.js

--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -19,7 +19,10 @@ module.exports = {
 	memberpermissions: [PermissionFlagsBits.SendMessages], //Only allow members with specific Permissions to execute a Commmand [OPTIONAL]
 	requiredroles: [], //Only allow specific Users with a Role to execute a Command [OPTIONAL]
 	alloweduserids: [], //Only allow specific Users to execute a Command [OPTIONAL]
-	options: [ //OPTIONAL OPTIONS, make the array empty / dont add this option if you don't need options!	
+	options: [
+		  {"User": { name: "user", description: "The user to kick.", required: true }},
+		 {"Strig": { name: "reason", description: "The reason for kick.", required: true }},
+		//OPTIONAL OPTIONS, make the array empty / dont add this option if you don't need options!	
 		//INFORMATIONS! You can add Options, but mind that the NAME MUST BE LOWERCASED! AND NO SPACES!!!, for the CHOCIES you need to add a array of arrays; [ ["",""] , ["",""] ] 
 		//{"Integer": { name: "ping_amount", description: "How many times do you want to ping?", required: true }}, //to use in the code: interacton.getInteger("ping_amount")
 		//{"String": { name: "ping_amount", description: "How many times do you want to ping?", required: true }}, //to use in the code: interacton.getString("ping_amount")
